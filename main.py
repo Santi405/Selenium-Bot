f6e8ebd333c4559aff2df0479e15ff367011cc37
Santi405 <zaninsantino@gmail.com>
2024-08-29 16:06:21 -0300

    Funciona para un solo link

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from selenium.webdriver.common.keys import Keys
 from materias import *
 import time
 
+nombres = ['"Zanin"', '"Brizio"', '"Waniewski"', '"Lanselotto"']
+
 service = Service(executable_path = "chromedriver.exe")
 driver = webdriver.Chrome(service = service)
 driver.get("https://qr-creator.com/urls/i8soKSkottLXL9ErLMoq0MtLLdFPyzc3isMibppSXhUHBEbeBsbajs6Ourpg0imxODM5sRhdODizuCQ1FygOBAA")
@@ -14,13 +16,22 @@ def click(x):
     time.sleep(1)
     link.click()
 
-click(BASICAS)
-click(AM2)
 
+
+for nombre in nombres:
+    driver.get("https://qr-creator.com/urls/i8soKSkottLXL9ErLMoq0MtLLdFPyzc3isMibppSXhUHBEbeBsbajs6Ourpg0imxODM5sRhdODizuCQ1FygOBAA")
+    click(BASICAS)
+    click(AM2)
+
+    time.sleep(2)
+    rB = driver.find_element(By.XPATH, "//input[@value="+ nombre +"]")
+    rB.click()
+
+    rB = driver.find_element(By.XPATH, "//*[@id='form-main-content1']/div/div/div[2]/div[2]/div/button")
+    rB.click()
+    time.sleep(2)
 
 time.sleep(10)
-
+# Cierra el WebDriver
 driver.quit()
-
-
 
--- pruebas.py
+++ pruebas.py
@@ -4,15 +4,20 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
+nombres = ['"Zanin"', '"Brizio"', '"Waniewski"', '"Lanselotto"']
+
 driver = webdriver.Chrome()
-driver.get("https://forms.office.com/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAZ__ufG30dUNFZKRDAwWTJLWTRaUUpNV01MQjNaTTdZMS4u")
-wait = WebDriverWait(driver, 5)
 
-rB = driver.find_element(By.XPATH, "//input[@value='Waniewski']")
-rB.click()
+for nombre in nombres:
+    driver.get("https://forms.office.com/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAZ__ufG30dUNFZKRDAwWTJLWTRaUUpNV01MQjNaTTdZMS4u")
 
-rB = driver.find_element(By.XPATH, "//*[@id='form-main-content1']/div/div/div[2]/div[2]/div/button")
-rB.click()
+    time.sleep(2)
+    rB = driver.find_element(By.XPATH, "//input[@value="+ nombre +"]")
+    rB.click()
+
+    rB = driver.find_element(By.XPATH, "//*[@id='form-main-content1']/div/div/div[2]/div[2]/div/button")
+    rB.click()
+    time.sleep(2)
 
 time.sleep(10)
 # Cierra el WebDriver
